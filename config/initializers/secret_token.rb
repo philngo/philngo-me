2a7afad83e01879a472a720e924895824953664a
Philip Ngo <ngo12@college.harvard.edu>
2013-02-13 14:54:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhilngoMe::Application.config.secret_token = '02bdd2b500152b586e3877c1c27745637791904ada78ca85f2d7f4a3245bab606e6b650f6da36efaa80fbd16640c0dd13a08c60df2414f32c08a6147479a5277'
