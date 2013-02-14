1f5545be0bda5530bda402e5870dd9e9bf9eb982
Philip Ngo <ngo12@college.harvard.edu>
2013-02-14 17:08:58 -0500

    photography

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ PhilngoMe::Application.routes.draw do
 
   match '/projects', to: 'static_pages#projects'
   match '/artwork', to: 'static_pages#artwork'
+  match '/photography', to: 'static_pages#photography'
   match '/calendar', to: 'static_pages#calendar'
 end
