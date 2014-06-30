bfad43ca791b5be2b0eb4ee3b606b14b90cb68d8
Phil Ngo <ngo.phil@gmail.com>
2014-06-30 00:30:18 -0500

    update website

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,13 @@
 class BlogController < ApplicationController
 
   def index
+   @posts = ["bezier","intro"] # recent first
    render "index", layout: "blog"
   end
 
   def show
-    index
+    @post = params["id"]
+    render "show", layout: "blog"
   end
 
 end
